72320acf5a83d5bd88d75a5b847f85ea92822125
bharatyadav11389 <122976307+bharatyadav11389@users.noreply.github.com>
2023-01-18 10:29:29 +0000

    Add files via upload

---
+++ 01_HelloWorld/src/com/citibank/main/HelloWorldMain.java
@@ -0,0 +1,15 @@
+package com.citibank.main;
+
+public class HelloWorldMain {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+		System.out.println("Welcome to citi" +   +100);
+		System.out.println("\"Welcome to citi\"");
+		
+	}
+	
+	public static void main(String Citibank) {
+		System.out.println("citi");
+	}
+
+}
